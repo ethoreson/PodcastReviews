79c0e684572dba16c3e66eb74ef797a9938b84f9
Emilie Thoreson and Laura Hamilton <laura@>
2017-07-12 13:35:11 -0700

    fix genre display

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,11 +19,17 @@ public class App {
   post("/", (request, response) -> {
     Map<String, Object> model = new HashMap<String, Object>();
     String name = request.queryParams("name");
-    String genre = request.queryParams("genre");
+    String genre = request.queryParams("podcastGenre");
     String description = request.queryParams("description");
     Podcast newPodcast = new Podcast(name, genre, description);
     newPodcast.save();
     response.redirect("/");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  get("/podcast/:id", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    model.put("podcasts", "templates/podcast.vtl");
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
